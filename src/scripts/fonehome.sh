fafa6285958c1eef6d18405f60949b3e12952695
archie.cobbs <archie.cobbs@0e0fb783-63aa-42d7-ae53-73cad7c4ef78>
2013-12-10 20:55:18 +0000

    - Add support for connecting to multiple servers. - Log connection attempts and status to syslog.
    Bug#:   1
    Bug#:   2

--- src/scripts/fonehome.sh
+++ src/scripts/fonehome.sh
@@ -6,8 +6,11 @@ NAME='@fonehomename@'
 USERNAME='@fonehomeuser@'
 CONFIG_FILE='@fonehomeconf@'
 KEY_FILE='@fonehomekey@'
-RETRY_DELAY="@fonehomeretry@"
-KNOWN_HOSTS_FILE="@fonehomehosts@"
+RETRY_DELAY='@fonehomeretry@'
+KNOWN_HOSTS_FILE='@fonehomehosts@'
+FACILITY_PATTERN='^(auth|authpriv|cron|daemon|ftp|lpr|mail|news|security|user|uucp|local[0-7])$'
+SYSLOG_TAG='@fonehomename@'
+SYSLOG_FACILITY='@fonehomelogfac@'
 
 # Usage message
 usage()
@@ -20,20 +23,36 @@ usage()
 }
 
 # Log functions
-log()
+perr()
 {
     echo ${NAME}: ${1+"$@"} 1>&2
+}
+
+log()
+{
+    LEVEL="${1}"
+    shift
+    logger -t "${SYSLOG_TAG}" -p "${SYSLOG_FACILITY}"."${LEVEL}" ${1+"$@"}
+    if [ -t 2 ]; then
+        perr ${1+"$@"}
+    fi
 }
 
 # Error function
 errout()
 {
-    log ${1+"$@"}
+    perr ${1+"$@"}
+    if ! [ -t 2 ]; then
+        log err ${1+"$@"}
+    fi
     exit 1
 }
 
 # Bail on errors
 set -e
+
+# Enable job control
+set -m
 
 # Parse flags passed in on the command line
 INITIALIZE="no"
@@ -75,56 +94,136 @@ case "${#}" in
         ;;
 esac
 
-# Read config file, which should set ${SSH_FLAGS}
+# Read config file, which should set configuration variables
 if ! [ -r "${CONFIG_FILE}" ]; then
     errout "can't read config file: ${CONFIG_FILE}"
 fi
 . "${CONFIG_FILE}"
 
 # Verify required fields are set
-if [ -z "${SERVER}" ]; then
-    errout "no server configured"
+if ! [[ -v SERVER ]]; then
+    errout "no server(s) configured"
 fi
-if [ -z "${USERNAME}" ]; then
-    errout "no username configured"
+if ! [[ -v USERNAME ]]; then
+    errout "no username(s) configured"
 fi
-if [ -z "${KEY_FILE}" -o ! -r "${KEY_FILE}" ]; then
-    errout "key file \`${KEY_FILE}' unreadable"
+if ! [[ -v KEY_FILE ]]; then
+    errout "no key file(s) configured"
 fi
+
+# Sanity check syslog facility name
+if ! echo "${SYSLOG_FACILITY}" | grep -qE "${FACILITY_PATTERN}"; then
+    errout "invalid syslog facility name \`${SYSLOG_FACILITY}'"
+fi
+
+# Make config variables into arrays
+declare -a SERVER USERNAME KEY_FILE SSH_FLAGS RETRY_DELAY
+
+# Get number of servers
+declare -i NUM_SERVERS="${#SERVER[@]}"
+declare -i i
+
+# Extend all arrays, repeating last element as necessary
+for ((i=0; $i < "${NUM_SERVERS}"; i++)); do
+    if (( "${#USERNAME[@]}" <= $i )); then
+        USERNAME[$i]="${USERNAME[$(($i - 1))]}"
+    fi
+    if (( "${#KEY_FILE[@]}" <= $i )); then
+        KEY_FILE[$i]="${KEY_FILE[$(($i - 1))]}"
+    fi
+    if (( "${#SSH_FLAGS[@]}" <= $i )); then
+        SSH_FLAGS[$i]="${SSH_FLAGS[$(($i - 1))]}"
+    fi
+    if (( "${#RETRY_DELAY[@]}" <= $i )); then
+        RETRY_DELAY[$i]="${RETRY_DELAY[$(($i - 1))]}"
+    fi
+done
+
+# Check required files
 if [ -z "${KNOWN_HOSTS_FILE}" ]; then
     errout "no known hosts file configured"
 fi
-if [ "${INITIALIZE}" != 'yes' ]; then
+if [ "${INITIALIZE}" = 'yes' ]; then
+    KNOWN_HOSTS_DIR=`dirname "${KNOWN_HOSTS_FILE}"`
+    if ! [ -w "${KNOWN_HOSTS_DIR}" ]; then
+        errout "known hosts file directory \`${KNOWN_HOSTS_DIR}' is unwritable"
+    fi
+else
     if ! [ -r "${KNOWN_HOSTS_FILE}" ]; then
         errout "known hosts file \`${KNOWN_HOSTS_FILE}' unreadable; did you run once with \`-I' flag?"
     fi
     if ! [ -s "${KNOWN_HOSTS_FILE}" ]; then
-        errout "known hosts file \`${KNOWN_HOSTS_FILE}' is empty; did you run once with \`-I' flag?"
+        errout "known hosts file \`${KNOWN_HOSTS_FILE}' is empty; did you run once with \`-I' flag for each server?"
     fi
 fi
+for ((i=0; $i < "${NUM_SERVERS}"; i++)); do
+    if ! [ -r "${KEY_FILE[$i]}" ]; then
+        errout "key file \`${KEY_FILE[$i]}' for server ${SERVER[$i]} is unreadable"
+    fi
+done
 
 # Doing initialization?
 if [ "${INITIALIZE}" = "yes" ]; then
-    exec ssh -24xaT -oStrictHostKeyChecking=ask -oUserKnownHostsFile="${KNOWN_HOSTS_FILE}" \
-      -oVisualHostKey=yes -oCheckHostIP=no -i "${KEY_FILE}" ${SSH_FLAGS} "${USERNAME}"@"${SERVER}"
+    for ((i=0; $i < "${NUM_SERVERS}"; i++)); do
+        perr "confirming server key for ${SERVER[$i]}"
+        ssh -24xaT -oStrictHostKeyChecking=ask -oUserKnownHostsFile="${KNOWN_HOSTS_FILE}" \
+          -oVisualHostKey=yes -oCheckHostIP=no -i "${KEY_FILE[$i]}" ${SSH_FLAGS[$i]} "${USERNAME[$i]}"@"${SERVER[$i]}"
+    done
+    exit 0
 fi
 
-# Ensure background process is killed when this script is killed
-set -m
-trap killchild TERM INT
-killchild()
+# Subshell - there is one of these for each server
+subshell()
 {
-    kill %% >/dev/null 2>&1
+    # Don't die on error
+    set +e
+
+    # Which server am I?
+    INDEX="${1}"
+
+    # Create temporary file to hold output of ssh(1) command
+    OUTPUT_FILE=`mktemp -q /tmp/fonehome.XXXXXXXX`
+    if [ $? -ne 0 ]; then
+        log error "can't create temporary file"
+        OUTPUT_FILE="/dev/null"
+    fi
+
+    # Clean up output file on exit
+    trap "if [ \"${OUTPUT_FILE}\" != '/dev/null' ]; then rm -f \"${OUTPUT_FILE}\"; fi; exit" INT TERM
+
+    # Connect to server; reconnect if we get disconnected
+    while true; do
+        log info initiating connection to "${SERVER[$INDEX]}"
+        ssh -24xaTnN -oBatchMode=yes -oExitOnForwardFailure=yes \
+          -oStrictHostKeyChecking=yes -oUserKnownHostsFile="${KNOWN_HOSTS_FILE}" \
+          -oServerAliveInterval=60 -oServerAliveCountMax=5 -oTCPKeepAlive=yes \
+          -oCheckHostIP=no -i "${KEY_FILE[$INDEX]}" ${SSH_FLAGS[$INDEX]} "${USERNAME[$INDEX]}"@"${SERVER[$INDEX]}" >"${OUTPUT_FILE}" 2>&1
+        if [ -s "${OUTPUT_FILE}" ]; then
+            log warn "connection to ${SERVER[$INDEX]} failed: `cat \"${OUTPUT_FILE}\"`"
+        else
+            log warn "connection to ${SERVER[$INDEX]} failed"
+        fi
+        sleep "${RETRY_DELAY[$INDEX]}"
+    done
+}
+
+# This function is used to ensure the subshells are killed when this script is killed
+killshells()
+{
+    jobs -p | sed 's/^/-/g' | xargs -r kill --
+    log info shutting down
+    wait
     exit
 }
 
-# Main loop
-while true; do
-    ssh -24xaTnN -oBatchMode=yes -oExitOnForwardFailure=yes \
-      -oStrictHostKeyChecking=yes -oUserKnownHostsFile="${KNOWN_HOSTS_FILE}" \
-      -oServerAliveInterval=60 -oServerAliveCountMax=5 -oTCPKeepAlive=yes \
-      -oCheckHostIP=no -i "${KEY_FILE}" ${SSH_FLAGS} "${USERNAME}"@"${SERVER}" >/dev/null 2>&1 &
-    wait
-    sleep ${RETRY_DELAY}
+# Clean up subshells on exit
+trap killshells INT TERM
+
+# Start the subshells
+for ((i=0; $i < "${NUM_SERVERS}"; i++)); do
+    subshell $i &
 done
+
+# Now we just hang out
+wait
 
